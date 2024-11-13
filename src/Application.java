f442a5f17268f950e7005b5104e0b741a233b97a
Timur Mansurov <Timur_Mansurov90@proton.me>
2024-11-13 21:13:12 +0900

    тест первого гита

---
+++ src/Application.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        // Логика меню
+        // 1. Шифрование
+        // 2. Расшифровка с ключом
+        // 3. Brute force
+        // 4. Статистический анализ
+        // 0. Выход
+        System.out.println("Шифровка методом Цезаря");
+        System.out.println("Выберите пункт меню");
+        System.out.println("1. Шифрование с ключом");
+        System.out.println("2. Расшифровка с ключом");
+        System.out.println("3. Brute force");
+        System.out.println("4. Статистический анализ");
+        System.out.println("0. Выход");
+
+        Scanner console = new Scanner(System.in);
+        int j = console.nextInt();
+
+//        switch (j) {
+//            case 1:
+//        }
+       // Add comment
+    }
+}
